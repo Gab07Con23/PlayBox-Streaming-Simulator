4b149aead1b55ab9918f30a7a5c998c032c2e958
GoDZz412 <minhnhat41203@gmail.com>
2026-04-15 20:41:24 -0400

    Commit first version of User and SubscriptionPlan classes

--- User.cs
+++ User.cs
@@ -8,6 +8,53 @@ namespace PlayBox
 {
     internal class User
     {
-        public string Name { get; protected set; }
+        // Private fields
+        private string name;
+        private SubscriptionPlan subscription;
+        private List<Content> watchHistory;
+        private List<Content> watchList;
+
+        // Constructor
+        public User(string name, SubscriptionPlan subscription)
+        {
+            this.name = name;
+            this.subscription = subscription;
+            watchHistory = new List<Content>();
+            watchList = new List<Content>();
+        }
+
+        // Properties
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public SubscriptionPlan Subscription
+        {
+            get { return subscription; }
+        }
+
+        // Methods
+        public void AddToWatchList(Content c)
+        {
+            watchList.Add(c);
+            Console.WriteLine($"{c.Title} added to watchlist.");
+        }
+
+        public void RateContent(Content c, double rating)
+        {
+            c.AddRating(rating);
+            Console.WriteLine($"{name} rated {c.Title} {rating}/10");
+        }
+
+        public List<Content> GetWatchHistory()
+        {
+            return watchHistory;
+        }
+
+        public void AddToHistory(Content c)
+        {
+            watchHistory.Add(c);
+        }
     }
 }
